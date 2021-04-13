6062b9c3729b851702f6e83bb0b0937178102273
Vince W <vincewallgren@me.com>
2021-04-13 04:21:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,4 +31,9 @@ class Song
   def self.find_by_name(name)
     self.all.find { |song| song.name == name }
   end
+
+  def self.find_or_create_by_name(name)
+    self.create_by_name(name) if !self.find_by_name(name)
+    self.find_by_name(name)
+  end
 end
